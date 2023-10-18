2848960842678a54aa8ddaece817fb45a2f398a7
Serge Moraru <90487457+Sergemoraru@users.noreply.github.com>
2023-10-18 06:32:04 -0600

    fixed npm issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import solidJs from "@astrojs/solid-js";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  site: "https://astrojs.org",
+  integrations: [solidJs(), react(), mdx()],
+  
+});
